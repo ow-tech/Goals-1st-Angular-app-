90973255a85bee3a440d9e344aa316281a80ebb3
ow-tech <wagwanwekon@outloo.com>
2020-03-25 16:22:49 +0300

    initial comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+// import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
+// import { FormsModule } from '@angular/forms';
+import { GoalFormComponent } from './goal-form/goal-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent,
+    GoalDetailComponent,
+    StrikethroughDirective,
+    DateCountPipe,
+    GoalFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
+  // imports: [
+  //   BrowserModule,
+  //   AppRoutingModule
+  //   FormsModule
+  // ],
   providers: [],
   bootstrap: [AppComponent]
 })
